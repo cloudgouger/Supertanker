63babfaa49a90469b9fb6162607538e244311118
kuregumo <189192350+cloudgouger@users.noreply.github.com>
2026-05-17 21:49:58 -0400

    Proper GUI and image on button support

--- main.py
+++ main.py
@@ -10,27 +10,49 @@ audioFiles = [
     "vine-boom.mp3"
 ]
 
+audioNames = [
+    "Squeaky Toy",
+    "MLG Airhorn",
+    "Cow Moo",
+    "Vine boom"
+]
+
+audioIcons = [
+    "rubber-duck.png",
+    "air-horn.png",
+    "cow.png",
+    "explosion.png"
+]
+
 def button_callback(sender, app_data, user_data):
     print(user_data)
     sfx, sR = sf.read(user_data)
     print(f"read {user_data}")
     sd.play(sfx)
-    sd.wait()
+    print("played sound")
+
+
 
 dpg.create_context()
 dpg.create_viewport()
 dpg.setup_dearpygui()
 
-with dpg.window(label="Supertanker"):
+for i, iconName in enumerate(audioIcons):
+    imageWidth, imageHeight, imageChannels, imageData = dpg.load_image(iconName)
+    with dpg.texture_registry():
+        dpg.add_static_texture(
+            width=imageHeight,
+            height=imageWidth,
+            default_value=imageData,
+            tag=iconName
+        )
+
+with dpg.window(tag="Supertanker"):
     dpg.add_text("Test")
-    dpg.add_button(label="Press me!", callback=button_callback, user_data=audioFiles[1])
+    for i, filename in enumerate(audioFiles):
+        dpg.add_image_button(audioIcons[i], label=audioNames[i], callback=button_callback, user_data=audioFiles[i], width=100, height=100)
+dpg.set_primary_window("Supertanker", True)
 dpg.show_viewport()
 dpg.start_dearpygui()
 dpg.destroy_context
 
-
-
-
-#print("Enter the number of a sound you'd like to play (1-4): ")
-#answer = input()
-#playAudio(audioFiles[int(answer)-1])
