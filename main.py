aa76f8a01f779da39a151170d8159c6580657cdd
kuregumo <189192350+cloudgouger@users.noreply.github.com>
2026-05-17 20:08:17 -0400

    add a button to play an sfx

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import numpy as np
 import sounddevice as sd
 import soundfile as sf
-import time
+import dearpygui.dearpygui as dpg
 
-def playAudio(filename):
-    sfx, sR = sf.read(filename)
-    print(f"read {filename}")
-    sd.play(sfx)
-    sd.wait()
 audioFiles = [
     "squeaky-toy_1.mp3",
     "mlg-airhorn.mp3",
@@ -15,6 +10,27 @@ audioFiles = [
     "vine-boom.mp3"
 ]
 
-print("Enter the number of a sound you'd like to play (1-4): ")
-answer = input()
-playAudio(audioFiles[int(answer)-1])
+def button_callback(sender, app_data, user_data):
+    print(user_data)
+    sfx, sR = sf.read(user_data)
+    print(f"read {user_data}")
+    sd.play(sfx)
+    sd.wait()
+
+dpg.create_context()
+dpg.create_viewport()
+dpg.setup_dearpygui()
+
+with dpg.window(label="Supertanker"):
+    dpg.add_text("Test")
+    dpg.add_button(label="Press me!", callback=button_callback, user_data=audioFiles[1])
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context
+
+
+
+
+#print("Enter the number of a sound you'd like to play (1-4): ")
+#answer = input()
+#playAudio(audioFiles[int(answer)-1])
