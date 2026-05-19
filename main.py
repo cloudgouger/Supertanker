ddc7695eccfd3929de8f1b1915ef7df088519c40
kuregumo <189192350+cloudgouger@users.noreply.github.com>
2026-05-18 21:31:15 -0400

    Add a working auto detection system to add your own sounds
    next: revamp system so it rechecks on boot, current system too finicky

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import soundfile as sf
 import dearpygui.dearpygui as dpg
 import os
 
-with open("audioFilesConfig.txt", 'w+') as audioFilesConfig:
-    audioFiles = audioFilesConfig.readlines()
-with open("audioNameConfig.txt", 'w+') as audioNameConfig:
-    audioNames = audioNameConfig.readlines()
-with open("audioIconConfig.txt", 'w+') as audioIconConfig:
-    audioIcons = audioIconConfig.readlines()
-buttons = []
-
+with open("audioFilesConfig.txt", 'r') as audioFilesConfig:
+    tempString = audioFilesConfig.read()
+    print(tempString)
+    audioFiles = tempString.splitlines()
+with open("audioNameConfig.txt", 'r') as audioNameConfig:
+    tempString = audioNameConfig.read()
+    audioNames = tempString.splitlines()
+with open("audioIconConfig.txt", 'r') as audioIconConfig:
+    tempString = audioIconConfig.read()
+    audioIcons = tempString.splitlines()
+print(audioFiles)
+print(audioIcons)
+audioFileExtensionless = []
+iconFileExtensionless = []
 
 
 def playSound(sender, app_data, user_data):
     print(user_data)
-    sfx, sR = sf.read(user_data)
+    sfx, sR = sf.read(f"data/sounds/{user_data}")
     print(f"read {user_data}")
     sd.play(sfx)
     print("played sound")
@@ -24,27 +30,25 @@ def addSound(sender, app_data, user_data):
     print(user_data)
 
 def reloadAudioFiles():
-    audioFileList = os.listdir("data/sounds")
-    iconFileList = os.listdir("data/icons")
-    audioFileExtensionless = []
-    iconFileExtensionless = []
-    for i, filename in enumerate(audioFileList):
+    audioFiles = os.listdir("data/sounds")
+    iconFiles = os.listdir("data/icons")
+    
+    
+    for i, filename in enumerate(audioFiles):
         name, fileExtension = os.path.splitext(filename)
         audioFileExtensionless.append(name)
-    for i, filename in enumerate(iconFileList):
+    for i, filename in enumerate(iconFiles):
         name, fileExtension = os.path.splitext(filename)
         iconFileExtensionless.append(name)
-    for i, filename in enumerate(audioFileList):
-        if audioFileExtensionless[i] == iconFileExtensionless[i]:
-            audioFiles.append(audioFileList[i])
-            audioIcons.append(iconFileList[i])
-    print(audioFileList)
-    print(iconFileList)
+    audioFiles.sort()
+    iconFiles.sort()
+    print(audioFiles)
+    print(iconFiles)
     with open("audioFilesConfig.txt", 'w') as audioFilesConfig:
-        for i in audioFileList:
+        for i in audioFiles:
             audioFilesConfig.write(f"{i}\n")
     with open("audioIconConfig.txt", 'w') as audioIconConfig:
-        for i in iconFileList:
+        for i in iconFiles:
             audioIconConfig.write(f"{i}\n")
 
 '''
@@ -84,8 +88,21 @@ dpg.create_viewport()
 dpg.setup_dearpygui()
 
 with dpg.window(tag="Supertanker"):
+    for i, iconName in enumerate(audioIcons):
+        print(f"loading {iconName}")
+        print(dpg.load_image(iconName))
+        imageWidth, imageHeight, imageChannels, imageData = dpg.load_image(f"data/icons/{iconName}")
+        with dpg.texture_registry():
+            dpg.add_static_texture(
+                width=imageHeight,
+                height=imageWidth,
+                default_value=imageData,
+                tag=iconName
+            )
+            print(f"added texture {iconName}")
     for i, filename in enumerate(audioFiles):
-        dpg.add_image_button(audioIcons[i], label=audioNames[i], callback=playSound, user_data=audioFiles[i], width=100, height=100)
+        print(audioIcons[i])
+        dpg.add_image_button(audioIcons[i], label="button", callback=playSound, user_data=audioFiles[i], width=100, height=100)
         print("added icon")
     dpg.add_button(label="Add sounds", callback=reloadAudioFiles, tag="add_button", parent="main_window")
     dpg.set_primary_window("Supertanker", True)
